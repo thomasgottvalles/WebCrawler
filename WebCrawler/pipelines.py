58bb9c2c4aa82979ae626a30637d555e6ed7d5aa
Thomas Gottvalles <tgottvalles@gmail.com>
2023-09-14 14:11:58 +0200

    feat: adds user inputs in main program and refactor the code

--- WebCrawler/pipelines.py
+++ WebCrawler/pipelines.py
@@ -1,10 +1,4 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+# -*- coding: utf-8 -*-
 from itemadapter import ItemAdapter
 from WebCrawler.items import PageItems, SiteItems
 import sqlite3
--- start.py
+++ start.py
@@ -2,12 +2,13 @@
 from scrapy.crawler import CrawlerProcess
 from WebCrawler.spiders.webcrawler import WebCrawler
 import sqlite3
-import subprocess
 
 
 if __name__ == "__main__": 
     
-    domain = input("Enter the first website domain the robot will crawl:")
+    print("=== WELCOME TO WEBCRAWLER ===")
+    print("About WebCrawler: https://github.com/thomasgottvalles/WebCrawler \n")
+    domain = input("Enter the first website domain the robot will crawl: ")
     process = CrawlerProcess()
     process.crawl(WebCrawler, domain=domain)
     process.start()
@@ -18,6 +19,13 @@ if __name__ == "__main__":
         cur.execute("SELECT domain FROM sites WHERE status=0 ORDER BY id LIMIT 1 ")
         domain = cur.fetchone()[0]
         con.commit()
-        print("Next website domain the robot will crawl:" + domain)
-        command = 'scrapy crawl webcrawler -a domain=' + domain
-        subprocess.run(command, shell=True)
+        print("\nNext website domain the robot will crawl: " + domain)
+        case = input("What do you want to do ? crawl, jump, exit: ")
+        if case == "crawl":
+            command = "scrapy crawl webcrawler -a domain=" + domain
+            subprocess.run(command, shell=True)
+        if case == "jump":
+            cur.execute("UPDATE sites SET status=1 WHERE domain='" + domain + "'")
+            con.commit()
+        if case == "exit":
+            break
